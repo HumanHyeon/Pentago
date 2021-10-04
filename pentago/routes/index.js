5f332b59a70d7bb8214ee105fe545ae41e4d3ed6
HumanHyeon <sehyeon1997@gmail.com>
2021-10-04 10:41:51 +0900

    change view ejs

---
+++ pentago/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+//DataBase Connect
+const config = require('../config/db')
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
+.then(() => console.log("DB Connected..."))
+.catch(err => console.log(err));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
