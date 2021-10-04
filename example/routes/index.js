4b8db59c039d95d354ef01a54bb0668416ef6c27
HumanHyeon <sehyeon1997@gmail.com>
2021-10-04 10:05:54 +0900

    db connected

--- example/routes/index.js
+++ example/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../config/db')
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
+.then(() => console.log("DB Connected..."))
+.catch(err => console.log(err));
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
